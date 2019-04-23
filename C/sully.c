21e919b542782d4aeb5ec9719f8e8b7ea7850f92
Louis Portay <lportay@student.42.fr>
2019-04-23 15:01:00 +0200

    Ruby and C versions locked

--- C/sully.c
+++ C/sully.c
@@ -8,8 +8,6 @@ i--;
 #endif
 char f[64]; sprintf(f,"Sully_%d.c",i);
 FILE *fs = fopen(f,"w"); if (!fs) {return -1;}
-char *s = "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$cint main() {%1$cint i = %2$d;%1$c#ifndef REPLICATE%1$ci--;%1$c#endif%1$cchar f[64]; sprintf(f,%3$cSully_%%d.c%3$c,i);%1$cFILE *fs = fopen(f,%3$cw%3$c); if (!fs) {return -1;}%1$cchar *s = %3$c%4$s%3$c;%1$cfprintf(fs,s,10,i,34,s); fclose(fs);%1$cif (i <= 0) {return 0;}%1$cchar o[64]; sprintf(o,%3$c./Sully_%%d%3$c,i);%1$cchar cmd[64]; sprintf(cmd,%3$cclang -Wall -Wextra -Werror %%s -o %%s%3$c,f,o + 2); system(cmd); system(o); }%1$c";
+char *s = "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$cint main() {%1$cint i = %2$d;%1$c#ifndef REPLICATE%1$ci--;%1$c#endif%1$cchar f[64]; sprintf(f,%3$cSully_%%d.c%3$c,i);%1$cFILE *fs = fopen(f,%3$cw%3$c); if (!fs) {return -1;}%1$cchar *s = %3$c%4$s%3$c;%1$cfprintf(fs,s,10,i,34,s); fclose(fs);%1$cif (i > 0) { char o[64]; sprintf(o, %3$cSully_%%d%3$c, i); char cmd[64]; sprintf(cmd,%3$cclang -Wall -Wextra -Werror %%s -o %%s && ./%%s%3$c,f,o,o); system(cmd); } }%1$c";
 fprintf(fs,s,10,i,34,s); fclose(fs);
-if (i <= 0) {return 0;}
-char o[64]; sprintf(o,"./Sully_%d",i);
-char cmd[64]; sprintf(cmd,"clang -Wall -Wextra -Werror %s -o %s",f,o + 2); system(cmd); system(o); }
+if (i > 0) { char o[64]; sprintf(o, "Sully_%d", i); char cmd[64]; sprintf(cmd,"clang -Wall -Wextra -Werror %s -o %s && ./%s",f,o,o); system(cmd); } }
--- Ruby/Sully.rb
+++ Ruby/Sully.rb
@@ -1,13 +1,18 @@
-#!/usr/bin/env
-
+#!/usr/bin/env ruby
 
 if __FILE__ == $0
-  i = 5 
+  i = 5
   i -= 1 if $DEBUG == true
-  return if i <= 0
-  File.open("Sully_#{i}.c", File::WRONLY|File::CREAT,0644) {|f|f.printf("#{PROG}",10,34,37,"#{PROG}",35)}}
+  s = %q(#!/usr/bin/env ruby
 
-
-
-
+if __FILE__ == $0
+  i = %d
+  i -= 1 if $DEBUG == true
+  s = %%q%c%s%c
+  File.open("Sully_#{i}.rb",File::WRONLY|File::CREAT,0644) {|f|f.printf(s,i,40,s,41)}
+  system("ruby -d Sully_#{i}.rb") if i > 0
+end
+)
+  File.open("Sully_#{i}.rb",File::WRONLY|File::CREAT,0644) {|f|f.printf(s,i,40,s,41)}
+  system("ruby -d Sully_#{i}.rb") if i > 0
 end
